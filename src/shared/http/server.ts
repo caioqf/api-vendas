e5cd864e3bff4cdb3351fbfea3d5083ff005dbb6
Caio <caio8200@gmail.com>
2022-02-14 13:49:06 -0300

    first commit

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, response, Response } from 'express';
 import cors from 'cors';
 import routes from './routes'
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -8,6 +9,19 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: 'error.message',
+    })
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+});
 
 app.listen(3333, () => {
   console.log('Server started on: http://localhost:3333');
