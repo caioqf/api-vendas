88b3feae44133d3c22418ca2d4e4c6fff7c79220
Caio <caio8200@gmail.com>
2022-02-16 11:45:41 -0300

    criado serviço para criar sessao

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,18 +1,23 @@
 import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
+import { sign } from 'jsonwebtoken';
 import User from "../typeorm/entities/User";
 import { UserRepository } from "../typeorm/repositories/UsersRepository";
 import bcryptjs from 'bcryptjs'
+
 
 interface IRequest {
   email: string;
   password: string;
 }
 
-
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionService {
-  public async execute({email, password}: IRequest): Promise<User> {
+  public async execute({email, password}: IRequest): Promise<IResponse> {
     const userRepository = getCustomRepository(UserRepository);
     
     const user = await userRepository.findByEmail(email);
@@ -27,7 +32,13 @@ class CreateSessionService {
       throw new AppError('Invalid credentials. Cod: 002', 401);
     }
 
-    return user;
+    const token = sign({}, '4304730ef8b98bff2878c84be7d80617', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {user, token};
   }
 }
+
  export default CreateSessionService;
